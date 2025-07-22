f310787862a8993de19cd788bc60020e978e3d90
robin <robin.noble@ynov.com>
2025-07-22 10:48:00 +0200

    .

--- moodle/caplogy_project/urls.py
+++ moodle/caplogy_project/urls.py
@@ -1,12 +1,15 @@
 
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from caplogy_app.views import login_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('caplogy_app.urls')),
+    path('login/', login_view, name='login'),
     path('', include('homepage.urls')),
     path('moodle/', include('caplogy_app.urls')),
     path('autodocs/', include('AutoDocs.myproject.urls')),
