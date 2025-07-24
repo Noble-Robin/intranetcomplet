17e8d38bf1d30a8ffe9dd4cc9c0c346e99e9641c
robin <robin.noble@ynov.com>
2025-07-24 11:11:50 +0200

    .

--- test_nextcloud_simple.py
+++ test_nextcloud_simple.py
@@ -13,10 +13,10 @@ def test_nextcloud_connection():
 
     # Liste de chemins à tester
     paths = [
-        "https://capdrive.caplogy.com:4443/remote.php/dav/files/r.noble/",  # chemin standard utilisateur
-        "https://capdrive.caplogy.com/remote.php/dav/files/r.noble",   # sans slash final
-        "https://capdrive.caplogy.com/remote.php/dav/files/DC89BA57-E31B-401B-81EB-B01276E0B218/", # chemin ID
-        "https://capdrive.caplogy.com/remote.php/dav/files/DC89BA57-E31B-401B-81EB-B01276E0B218",  # ID sans slash
+        "http://capdrive.caplogy.com:443/remote.php/dav/files/r.noble/",  # chemin standard utilisateur
+        "http://capdrive.caplogy.com:443/remote.php/dav/files/r.noble",   # sans slash final
+        "http://capdrive.caplogy.com:443/remote.php/dav/files/DC89BA57-E31B-401B-81EB-B01276E0B218/", # chemin ID
+        "http://capdrive.caplogy.com:443/remote.php/dav/files/DC89BA57-E31B-401B-81EB-B01276E0B218",  # ID sans slash
     ]
 
     for url in paths:
